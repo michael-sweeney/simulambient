0c4e3555a12a4919be9f6d9fb3e6cc33ae60ea7d
Mike Sweeney <mikeswny@umich.edu>
2022-11-26 15:26:24 -0500

    Added comments to estimate_params for context/readability

--- R/estimate_params.R
+++ R/estimate_params.R
@@ -56,40 +56,45 @@ estimate_params <- function(dataset, cellTypes = NULL, batch = NULL, sample = NU
 
   l <- length(metadata) # How many metadata parameters did the user include?
 
-  if (l == 0) {
+  if (l == 0) { # Metadata-free case. Get rowMeans and UMIs per droplet and return.
     genedf <- rowMeans(dataset)
     names(genedf) <- rownames(dataset)
     umis <- colSums(dataset)
     return(list(geneMeans = matrix(genedf, nrow = length(genedf)), umis = matrix(umis, nrow = length(umis))))
   } else {
-    metadata_df <- data.frame(mget(metadata))
 
-    tbl <- as.data.frame(data.table(metadata_df)[, .N, by = metadata])
+    metadata_df <- data.frame(mget(metadata)) # Make dataframe of metadata objects
 
-    if (l == 1) {
+    tbl <- as.data.frame(data.table(metadata_df)[, .N, by = metadata]) # Going to iterate over all unique permutations of metadata combinations
+
+    # Point of this if/else switch is to find droplets in dataset corresponding to particular metadata permutation
+    if (l == 1) { # One of cellTypes, batch, sample provided
       metadata_df_concat <- metadata_df[,1]
       tbl_concat <- tbl[,1]
-    } else if (l == 2) {
+    } else if (l == 2) { # Two provided
       metadata_df_concat <- paste(metadata_df[,1], metadata_df[,2], sep = "")
       tbl_concat <- paste(tbl[,1], tbl[,2], sep = "")
-    } else {
+    } else {# Three provided
       metadata_df_concat <- paste(metadata_df[,1], metadata_df[,2], metadata_df[,3], sep = "")
       tbl_concat <- paste(tbl[,1], tbl[,2], tbl[,3], sep = "")
     }
 
+    # Initialize matrices for function result
     genedf <- matrix(NA, ncol = nrow(tbl), nrow = eval(nrow(dataset) + l))
     umis <- matrix(NA, ncol = nrow(tbl), nrow = eval(l + max(tbl[,eval(l+1)])))
 
-    genedf[1:l,] <- t(tbl[,1:l])
+    genedf[1:l,] <- t(tbl[,1:l]) # Fill genedf with metadata information
 
-    umis[1:l,] <- t(tbl[,1:l])
+    umis[1:l,] <- t(tbl[,1:l]) # Finll umis with metadata information
 
+    # Fill genedf and umis with gene means and UMI information
     for (i in 1:nrow(tbl)) {
       genedf[eval(l+1):eval(nrow(dataset) + l),i] <- rowMeans(dataset[,which(metadata_df_concat == tbl_concat[i])])
       umiVector <- colSums(dataset[,which(metadata_df_concat == tbl_concat[i])])
       umis[eval(l+1):eval(l+length(umiVector)),i] <- umiVector
     }
 
+    # Give metadata rownames
     rownames(genedf) <- c(colnames(metadata_df), rownames(dataset))
     rownames(umis) <- c(colnames(metadata_df), as.character(1:eval(nrow(umis)-2)))
 
