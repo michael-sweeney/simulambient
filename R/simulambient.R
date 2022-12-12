23db2a40f6b1a5b408714c1d4a93221bd99c8c43
Mike Sweeney <mikeswny@umich.edu>
2022-12-12 15:32:08 -0500

    Updated main function, now handles the metadata groups as opposed to the helper functions

--- R/simulambient.R
+++ R/simulambient.R
@@ -8,13 +8,13 @@
 #'
 #' @param contamination An n x j matrix-like object where n = number of genes and j = number of ambient mRNA droplets. The droplets in this matrix are believed to be by the user to be composed of ambient mRNA. Including droplets beneath a specific UMI cutoff (i.e. 100) is a basic idea to get ambient mRNA droplets.
 #'
-#' @param cellTypes A vector of length k that provides the cell type of each nucleus in \code{dataset}. If all nuclei are from one type or cell type is not to be accounted for, parameter can be left blank.
+#' @param metadata A matrix with k rows that corresponds to metadata of the k nuclei provided in \code{dataset}. Please see \code{cellTypes}, \code{batch}, and \code{bgBatch}. Each column should represent a different aspect of metadata, i.e. sex. It should be noted that the user should be rather parsimonious when choosing which metadata to include. Including too many parameters will increase the number of metadata permutations, which will increase runtime and potentially "overfit" the simulations. Cell types is strongly recommended, anything else is up to the user's discretion.
 #'
-#' @param batch A vector of length k that provides the batch of the nucleus in \code{dataset}. If there is no batch effect, parameter can be left blank. This parameter must be provided if \code{bgBatch} is provided (as required by DecontX).
+#' @param cellTypes A vector of length k should cell type information not be provided in \code{metadata}. If it is provided, this parameter should be a string of the column name that represents the cell type information in \code{metadata}. If cell type information is not desired to be included, default is NULL.
+#'
+#' @param batch A vector of length k that provides the batch of the nucleus in \code{dataset}. If there is no batch effect, parameter can be left blank. This parameter must be provided if \code{bgBatch} is provided (as required by DecontX). Please do not include batch information in the \code{metadata} parameter.
 #'
 #' @param bgBatch A vector of length j corresponding to batch information of the ambient mRNA droplets in \code{contamination}. This parameter must be provided if \code{batch} is provided (as required by DecontX).
-#'
-#' @param sex A vector of length k that provides the associated sex of each droplet in \code{dataset}.
 #'
 #' @param seed Seed for random number generation. Can be set by the user. Defaults to 615.
 #'
@@ -25,16 +25,36 @@
 #' @import stats
 
 
-simulambient <- function(dataset, contamination_levels = 1, contamination = NULL, cellTypes = NULL, batch = NULL, bgBatch = NULL, sex = NULL, seed = 615, ...) {
+simulambient <- function(dataset, contamination_levels = 1, contamination = NULL, metadata = NULL, cellTypes = NULL, batch = NULL, bgBatch = NULL, seed = 615, ...) {
 
-  ## STEPS 0A-0C: CHECK ARGUMENTS FOR VALIDITY & SAVE UMI INFORMATION
+  ## STEPS 0A-0C: CHECK ARGUMENTS FOR VALIDITY, CONFIGURE METADATA GROUPS, REMOVE GENES WITHOUT EXPRESSION
 
-  print("Checking arguments for validity...")
+  message("Checking arguments for validity...")
 
-  metadata <- check_args(dataset = dataset, contamination = contamination, cellTypes = cellTypes, batch = batch, bgBatch = bgBatch, sex = sex)
-  preDecontX_umis <- get_umis(dataset = dataset, metadata = metadata, cellTypes = cellTypes, sex = sex)
+  metadata <- check_args(dataset = dataset, contamination = contamination, metadata = metadata, cellTypes = cellTypes, batch = batch, bgBatch = bgBatch)
 
-  print("Removing genes with no expression...")
+  message("Handling metadata permutations...")
+
+  # metadata_indices --> Based on the user-input dataset, which nucleus belongs to which metadata group?
+  # metadata_indices_sorted --> Used to sort user-input dataset to organize it so metadata groups are next to each other
+  # metadata_indices_original --> If we sort the user-input dataset, we use this to "unsort" it and get back to original order.
+
+  metadata_tbl <- unique(metadata) # Get unique permutations
+  metadata_indices <- match(do.call(paste, data.frame(metadata)), do.call(paste, data.frame(metadata_tbl))) # Assign each nucleus to its metadata permutation
+
+  metadata_sort_df <- data.frame(metadata_indices, 1:length(metadata_indices))
+  metadata_indices_sorted <- metadata_sort_df[order(metadata_sort_df[,1]),]
+  metadata_indices_sorted_df <- data.frame(metadata_indices_sorted, 1:nrow(metadata_indices_sorted))
+  metadata_indices_sorted <- metadata_indices_sorted[,2]
+
+  metadata_indices_sorted_df <- metadata_indices_sorted_df[order(metadata_indices_sorted_df[,2]),]
+  metadata_indices_original <- metadata_indices_sorted_df[,3]
+
+  num_nuclei_per_metadata_group <- as.data.frame(table(metadata_indices))[,2]
+
+  preDecontX_umis <- colSums(dataset)[metadata_indices_sorted] # NOW IN METADATA ORDER
+
+  message("Removing genes with no expression...")
 
   expressed_genes <- which(rowSums(dataset) > 0)
   dataset <- dataset[expressed_genes,]
@@ -44,77 +64,71 @@ simulambient <- function(dataset, contamination_levels = 1, contamination = NULL
 
   ## STEP 1: DECONTAMINATE
 
-  print("Decontaminating snRNA-seq data with DecontX...")
+  message("Decontaminating snRNA-seq data with DecontX...")
 
-  decontaminated_counts <- decontaminate(dataset, contamination, cellTypes, batch, ...)
+  if (!is.null(cellTypes)) {
+    cellTypes <- metadata[,"cellTypes"]
+  }
 
-  return(decontaminated_counts)
+  decontaminated_counts <- decontaminate(dataset, contamination, cellTypes, batch, bgBatch, ...)
+
+  postDecontX_umis <- colSums(decontaminated_counts)[metadata_indices_sorted] # NOW IN METADATA ORDER
 
   ## STEP 2: ESTIMATE PARAMETERS
 
-  print("Estimating snRNA-seq data gene expression profile...")
+  message("Estimating snRNA-seq data gene expression profile...")
 
-  params <- estimate_params(dataset = decontaminated_counts, metadata = metadata, cellTypes = cellTypes, sex = sex)
+  params <- estimate_params(dataset = decontaminated_counts, metadata = metadata, metadata_indices = metadata_indices)
 
   ## STEP 3: SIMULATE GROUND TRUTH
 
-  print("Simulating ground truth...")
+  message("Simulating ground truth...")
 
-  umis <- params$umis
-  num_nuclei_per_metadata <- apply(umis, MARGIN = 2, FUN = function(x) length(stats::na.omit(x)))
+  counts <- simulate_counts(geneMeans = params$geneMeans, geneSDs = params$geneSDs, numNucleiPerGroup = num_nuclei_per_metadata_group, seed = seed)
 
-  counts <- simulate_counts(geneMeans = params$geneMeans, geneSDs = params$geneSDs, numNucleiPerGroup = num_nuclei_per_metadata, seed = seed)
+  rownames(counts) <- rownames(dataset)
+  colnames(counts) <- as.character(1:ncol(counts))
+
+  ground_truth_output <- round(counts)
+  ground_truth_output <- ground_truth_output[,metadata_indices_original]
+  output_list <- list(ground_truth_output)
 
   ## STEP 4: SCALE GROUND TRUTH
 
-  print("Applying nuclei-specific scale factors...")
+  message("Applying nuclei-specific scale factors...")
 
-  umis_vector <- na.omit(as.vector(umis))
-  scaling_factor <- umis_vector / colSums(counts)
+  scaling_factor <- postDecontX_umis / colSums(counts)
   scaling_factor[!is.finite(scaling_factor)] <- 0
 
-  counts <- sweep(counts, 2, scaling_factor, FUN = "/") # Ground truth counts
-  counts <- round(counts)
-
-  output_list <- list(counts)
+  counts <- t(t(counts) * scaling_factor)
 
   ## STEP 5: GET AMBIENT MRNA PROFILE MEANS
 
-  print("Estimating ambient mRNA droplets gene expression profile...")
+  message("Estimating ambient mRNA droplets gene expression profile...")
 
-  if (!is.null(bgBatch)) {
-    metadata2 <- "cellTypes" # using this parameter as batch substitute
-  } else {
-    metadata2 <- c()
-  }
-  contamination_params <- estimate_params(dataset = contamination, metadata = metadata2, cellTypes = bgBatch, sex = NULL)
-
-  contamination_umis <- contamination_params$umis
-  if (is.null(batch)) {
-    num_droplets_per_metadata <- c(ncol(counts))
-  } else {
-    ## TODO: Fix this
-    num_droplets_per_metadata <- as.data.frame(table(batch))[,2]
-  }
-
-  print("Simulating contamination counts...")
-
-  contamination_counts <- simulate_counts(geneMeans = contamination_params$geneMeans * 100, geneSDs = contamination_params$geneSDs, numNucleiPerGroup = num_droplets_per_metadata, seed = seed)
+  contamination_params <- estimate_ambient_params(contamination = contamination, bgBatch = bgBatch)
 
   ## STEP 6: ADD DESIRED CONTAMINATION LEVELS TO GROUND TRUTH DATA
 
-  preDecontX_umis_vector <- na.omit(as.vector(preDecontX_umis))
-
-  contamination_to_add_vector <- preDecontX_umis_vector - umis_vector
-
   for (i in 1:length(contamination_levels)) {
-    scaling_factor <- contamination_to_add_vector / colSums(contamination_counts)
-    scaling_factor[!is.finite(scaling_factor)] <- 0
+    message(paste("Simulating contamination counts at factor ", contamination_levels[i], "...", sep = ""))
 
-    contamination_counts <- sweep(contamination_counts, 2, scaling_factor, FUN = "/") # Contaminated counts
-    contamination_counts <- contamination_counts * contamination_levels[i]
-    contamination_counts <- round(contamination_counts)
-    final_matrix <- counts + contamination_counts
+    contamination_to_add_vector <- preDecontX_umis - postDecontX_umis
+    contamination_to_add_vector <- contamination_to_add_vector * contamination_levels[i]
+
+    contamination_counts <- simulate_contamination(geneMeans = contamination_params, numCounts = contamination_to_add_vector, batch = batch, seed = seed)
+
+    # Scale down ground truth counts to account for UMI displacement
+    scaling_factor <- (preDecontX_umis - contamination_to_add_vector) / colSums(counts)
+    scaling_factor[!is.finite(scaling_factor)] <- 0
+    scaling_factor[scaling_factor < 0] <- 0
+    counts_temp <- t(t(counts) * scaling_factor)
+
+    final_matrix <- counts_temp + contamination_counts
+    final_matrix <- round(final_matrix)
+    final_matrix <- final_matrix[,metadata_indices_original]
+    rownames(final_matrix) <- rownames(dataset)
+    colnames(final_matrix) <- as.character(1:ncol(final_matrix))
     output_list <- append(output_list, final_matrix)
   }
 
